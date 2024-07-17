3d67d9a2d9ef7d2c806447b273ac5e6423b42e6d
Shadowsweep <utkarshhg911@gmail.com>
2024-07-17 16:56:42 +0530

    Arrays questions

---
+++ array/secondlargestarray.cpp
@@ -0,0 +1,30 @@
+
+
+// ---------------- brute force approach -----------------
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void getSecond(int arr[],int n){
+//     if(n==0 || n==1){
+//         cout<<-1<<" "<<-1<<endl;
+
+//     }
+//     sort(arr,arr+n);
+//     cout<<"The second smallest iss :"<<arr[1]<<endl;
+//     cout<<"The second largest iss :"<<arr[n-2];
+
+
+// }
+
+// int main(){
+//     int arr[] ={1,2,3,3,7,4};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     getSecond(arr,n);
+//     return 0;
+// }
+
+
+// --------------------------------- Better Approach ----------------------------
+
+
---
+++ insertionsort.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+void selection_sort(int arr[], int n) {
+  // selection sort
+  for (int i = 0; i < n - 1; i++) {
+    int mini = i;
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[mini]) {
+        mini = j;
+      }
+    }
+    int temp = arr[mini];
+    arr[mini] = arr[i];
+    arr[i] = temp;
+  }
+
+  cout << "After selection sort: " << "\n";
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << "\n";
+
+}
+
+
+void bubble_sort(int arr[], int n) {
+    // bubble sort
+    for (int i = n - 1; i >= 0; i--) {
+        int didSwap = 0;
+        for (int j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                didSwap = 1;
+            }
+        }
+        if(didSwap == 0) break;
+    }
+
+    cout << "After Using bubble sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+void insertion_sort(int arr[] ,int n ){
+    for(int i=0;i<=n-1;i++)
+    { int j = i;
+      while(j>0 && arr[j-1]>arr[j]){
+        swap(arr[j-1],arr[j]);
+        j--;
+      }
+
+    }
+     cout << "After Using insertion sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+  int arr[] = {9,8,5,2,1};
+  int n = sizeof(arr) / sizeof(arr[0]);
+   cout << "Before selection sort: " << "\n";
+   for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << "\n";
+  insertion_sort(arr, n);
+  return 0;
+}

5721c2df2d564ddbc5ffbe354edeb3084b0023db
Shadowsweep <utkarshhg911@gmail.com>
2024-09-05 15:18:32 +0530

    strings folder

---
+++ Strings/howtotakingstringinput.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+   char name[10];
+   cout<<"Enter your name"<<endl;
+   cin>>name;
+   cout<<"Your name is: "<<name;
+   
+   
+
+    return 0;
+}
